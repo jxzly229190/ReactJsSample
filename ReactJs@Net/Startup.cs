4e4209a95f908792aa8a09e4f42726f4a16cae14
jxzly229190 <jx-zly@live.cn>
2015-08-23 22:57:42 +0800

    initial data

---
+++ ReactJs@Net/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReactJs_Net.Startup))]
+namespace ReactJs_Net
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
